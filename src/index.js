88e4d4f0d22a6dd4d3edfdbde2c6fa671273037f
Vladislav1552 <vladiskav333@gmail.com>
2018-09-16 18:46:19 +0300

    5 task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
  var currencyy={
      H:50,
      Q:25,
      D:10,
      N:5,
-     P:1
+     P:1,
  } 
- var result= {}
+ var result= {};
  if(currency <= 0) return {};
  if(currency >= 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
  if(Math.floor(currency/currencyy.H)){
